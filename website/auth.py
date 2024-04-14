cd15016f9054bf01447c2cac30ec071d8815fe7b
Omsurvase24 <omsurvase24@gmail.com>
2024-04-15 03:48:55 +0530

    signup form

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("login.html")
 
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
